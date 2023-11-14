b7579efa97ea05677833857690e6243da1a1dba9
rayhankinan <rayhankinan@gmail.com>
2023-11-14 19:04:03 +0700

    Run prisma on bun

--- ticket-service/src/index.ts
+++ ticket-service/src/index.ts
@@ -1,6 +1,12 @@
 import { Elysia } from "elysia";
+import { PrismaClient } from "./generated/client";
 
-const app = new Elysia().listen(3000);
+const app = new Elysia()
+  .decorate("db", new PrismaClient())
+  .group("/events", (app) =>
+    app.get("/", async ({ db }) => await db.event.findMany())
+  )
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
