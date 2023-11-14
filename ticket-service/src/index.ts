fe78b130b673b940ddeaae7a5a62a5d3afe010c5
rayhankinan <rayhankinan@gmail.com>
2023-11-14 22:45:50 +0700

    Add event CRUD

--- ticket-service/src/index.ts
+++ ticket-service/src/index.ts
@@ -1,10 +1,71 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { PrismaClient } from "@prisma/client";
 
 const app = new Elysia()
   .decorate("db", new PrismaClient())
   .group("/events", (app) =>
-    app.get("/", async ({ db }) => await db.event.findMany())
+    app
+      .get("/", async ({ db }) => await db.event.findMany())
+      .get(
+        "/:id",
+        async ({ db, params }) =>
+          await db.event.findUnique({
+            where: params,
+          }),
+        {
+          params: t.Object({ id: t.String() }),
+        }
+      )
+      .post(
+        "/",
+        async ({ db, body }) =>
+          await db.event.create({
+            data: body,
+          }),
+        {
+          body: t.Object({
+            name: t.String(),
+            lineup: t.Array(t.String()),
+            description: t.Optional(t.String()),
+            homepage: t.Optional(t.String()),
+            startTime: t.String({ format: "date-time" }),
+            endTime: t.String({ format: "date-time" }),
+          }),
+        }
+      )
+      .put(
+        "/:id",
+        async ({ db, params, body }) =>
+          await db.event.update({
+            where: params,
+            data: body,
+          }),
+        {
+          body: t.Object({
+            name: t.String(),
+            lineup: t.Array(t.String()),
+            description: t.Optional(t.String()),
+            homepage: t.Optional(t.String()),
+            startTime: t.String({ format: "date-time" }),
+            endTime: t.String({ format: "date-time" }),
+          }),
+          params: t.Object({
+            id: t.String(),
+          }),
+        }
+      )
+      .delete(
+        "/:id",
+        async ({ db, params }) =>
+          await db.event.delete({
+            where: params,
+          }),
+        {
+          params: t.Object({
+            id: t.String(),
+          }),
+        }
+      )
   )
   .listen(3000);
 
