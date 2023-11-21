5f50e677d09cc543ffa73698c805cb5e5fcbb137
acomarcho <acoxstpd@gmail.com>
2023-11-22 06:48:42 +0700

    Refactor: main.ts

--- payment-service/main.ts
+++ payment-service/main.ts
@@ -2,18 +2,21 @@ import { redisClient } from "./redis/index.ts";
 import { expressApp } from "./express/index.ts";
 import { processInvoice } from "./helpers/process-invoice.ts";
 
+const main = async () => {
+  await connectAndSubscribeToPubsub();
+  startExpressServer();
+};
+
 const connectAndSubscribeToPubsub = async () => {
   const subscriber = redisClient.duplicate();
-
   await subscriber.connect();
-
   await subscriber.subscribe("payment", processInvoice);
 };
 
-(async () => {
-  await connectAndSubscribeToPubsub();
-
+const startExpressServer = () => {
   expressApp.listen(3002, () => {
     console.log(">> Payment service is up and running!");
   });
-})();
+};
+
+main();
