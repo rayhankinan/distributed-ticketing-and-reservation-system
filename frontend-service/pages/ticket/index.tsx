109f0e760e88808cc94a06cbef5d376c41c20e7a
acomarcho <acoxstpd@gmail.com>
2023-11-27 17:42:06 +0700

    Feat: Finish ticket booking page

--- frontend-service/pages/ticket/index.tsx
+++ frontend-service/pages/ticket/index.tsx
@@ -1,6 +1,5 @@
-import { Divider, Button } from "@nextui-org/react";
-import { Select, SelectSection, SelectItem } from "@nextui-org/react";
-import Link from "next/link";
+import { Divider, Button, Card, CardBody, CardHeader } from "@nextui-org/react";
+import { Select, SelectItem } from "@nextui-org/react";
 
 const dummyEvents = [
   {
@@ -17,38 +16,84 @@ const dummyEvents = [
   },
 ];
 
+const dummySeats = [
+  {
+    id: 1,
+    name: "A1",
+  },
+  {
+    id: 2,
+    name: "A2",
+  },
+  {
+    id: 3,
+    name: "A3",
+  },
+];
+
 export default function Page() {
   return (
-    <div className="max-w-[1160px] mx-auto p-[1rem] py-[2rem] min-h-screen">
-      <div className="flex flex-col gap-[0.5rem]">
-        <h1 className="font-bold text-[2rem]">Pemesanan Tiket Terdistribusi</h1>
-        <p>
-          Pilih suatu event, lalu pilih seat yang Anda inginkan untuk memesan
-          tiket!
-        </p>
-      </div>
-      <Divider className="mt-[1rem]" />
-      <Select
-        label="Pilih event"
-        className="dark"
-        classNames={{
-          listboxWrapper: "dark",
-          listbox: "dark",
-          popoverContent: "dark",
-        }}
-      >
-        {dummyEvents.map((e) => {
-          return (
-            <SelectItem
-              key={e.id}
-              value={e.id}
-              className="dark text-foreground"
-            >
-              {e.name}
-            </SelectItem>
-          );
-        })}
-      </Select>
+    <div className="max-w-[1160px] mx-auto p-[1rem] py-[2rem] min-h-screen flex items-center">
+      <Card className="p-[1rem] w-full">
+        <CardHeader>
+          <div className="flex flex-col gap-[0.5rem]">
+            <h1 className="font-bold text-[2rem]">
+              Pemesanan Tiket Terdistribusi
+            </h1>
+            <p>
+              Pilih suatu event, lalu pilih seat yang Anda inginkan untuk
+              memesan tiket!
+            </p>
+          </div>
+        </CardHeader>
+        <Divider className="my-[1rem]" />
+        <CardBody>
+          <Select
+            label="Pilih event"
+            className="dark"
+            classNames={{
+              listboxWrapper: "dark",
+              listbox: "dark",
+              popoverContent: "dark",
+            }}
+          >
+            {dummyEvents.map((e) => {
+              return (
+                <SelectItem
+                  key={e.id}
+                  value={e.id}
+                  className="dark text-foreground"
+                >
+                  {e.name}
+                </SelectItem>
+              );
+            })}
+          </Select>
+          <Divider className="my-[1rem]" />
+          <Select
+            label="Pilih seat"
+            className="dark"
+            classNames={{
+              listboxWrapper: "dark",
+              listbox: "dark",
+              popoverContent: "dark",
+            }}
+          >
+            {dummySeats.map((e) => {
+              return (
+                <SelectItem
+                  key={e.id}
+                  value={e.id}
+                  className="dark text-foreground"
+                >
+                  {e.name}
+                </SelectItem>
+              );
+            })}
+          </Select>
+          <Button className="mt-[1rem] w-full">Reservasi seat</Button>
+        </CardBody>
+      </Card>
     </div>
   );
 }
