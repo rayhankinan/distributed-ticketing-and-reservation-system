4942e4cf1ed70217b6acc5a8fdb92b253097912b
acomarcho <acoxstpd@gmail.com>
2023-11-28 20:19:35 +0700

    Feat: Make login form controllable

--- frontend-service/pages/index.tsx
+++ frontend-service/pages/index.tsx
@@ -1,10 +1,29 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Input, Divider, Button, CardFooter } from "@nextui-org/react";
 import { Card, CardHeader, CardBody } from "@nextui-org/react";
 import Link from "next/link";
 
 import Router from "next/router";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+
+const loginSchema = z.object({
+  username: z.string().min(1),
+  password: z.string().min(1),
+});
+
+type LoginSchemaType = z.infer<typeof loginSchema>;
 
 export default function Page() {
+  const {
+    control,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm<LoginSchemaType>({
+    resolver: zodResolver(loginSchema),
+    mode: "onChange",
+  });
+
   const handleLogIn = () => {
     Router.push("/home");
   };
@@ -21,12 +40,39 @@ export default function Page() {
         <Divider className="my-[1rem]" />
         <CardBody>
           <div className="flex flex-col gap-[1rem]">
-            <Input type="text" label="Username" placeholder="johndoe" />
-            <Input type="password" label="Password" />
+            <Controller
+              control={control}
+              name="username"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  type="text"
+                  label="Username"
+                  placeholder="johndoe"
+                  value={value}
+                  onValueChange={onChange}
+                  className="w-full"
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="password"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  type="password"
+                  label="Password"
+                  value={value}
+                  onValueChange={onChange}
+                  className="w-full"
+                />
+              )}
+            />
             <Button
               onClick={() => {
                 handleLogIn();
               }}
+              isDisabled={!isValid}
+              className="w-full"
             >
               Log in
             </Button>
--- frontend-service/pages/register/index.tsx
+++ frontend-service/pages/register/index.tsx
@@ -96,7 +96,6 @@ export default function Page() {
                 />
               )}
             />
-
             <Button
               onClick={() => {
                 handleSubmit(onRegister)();
