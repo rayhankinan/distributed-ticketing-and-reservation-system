21e27ddc0aa27e22dc2af2bcf0aef0db0b73c693
acomarcho <acoxstpd@gmail.com>
2023-11-28 20:38:59 +0700

    Feat: Add redux-persist

--- frontend-service/pages/_app.tsx
+++ frontend-service/pages/_app.tsx
@@ -2,17 +2,20 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { NextUIProvider } from "@nextui-org/react";
 import { Toaster } from "sonner";
-import { store } from "@/redux/store";
+import { persistor, store } from "@/redux/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
       <Provider store={store}>
-        <main className="dark text-foreground bg-background">
-          <Toaster />
-          <Component {...pageProps} />
-        </main>
+        <PersistGate loading={null} persistor={persistor}>
+          <main className="dark text-foreground bg-background">
+            <Toaster />
+            <Component {...pageProps} />
+          </main>
+        </PersistGate>
       </Provider>
     </NextUIProvider>
   );
--- frontend-service/redux/store.ts
+++ frontend-service/redux/store.ts
@@ -1,10 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "./slices/user";
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedUserReducer = persistReducer(persistConfig, userReducer);
+
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: { user: persistedUserReducer },
 });
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
