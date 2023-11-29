146235bc40cda4a014394a8ef9bf29c520f037a9
acomarcho <acoxstpd@gmail.com>
2023-11-29 11:44:39 +0700

    Feat: Add logger at payment service

--- payment-service/express/index.ts
+++ payment-service/express/index.ts
@@ -2,27 +2,32 @@
 import express from "npm:express";
 // @deno-types="npm:@types/body-parser"
 import bodyParser from "npm:body-parser";
+import morgan from "npm:morgan";
 import { expressjwt } from "npm:express-jwt";
 
 import { createInvoiceHandler } from "./invoice-handler.ts";
 import { createRefundHandler } from "./refund-handler.ts";
 
-export const expressApp = express()
-  .post(
-    "/invoice",
-    bodyParser.json(),
-    expressjwt({
-      secret: "dhika-jelek",
-      algorithms: ["HS256"],
-    }),
-    createInvoiceHandler
-  )
-  .post(
-    "/refund",
-    bodyParser.json(),
-    expressjwt({
-      secret: "dhika-jelek",
-      algorithms: ["HS256"],
-    }),
-    createRefundHandler
-  );
+export const expressApp = express();
+
+expressApp.use(morgan("tiny"));
+
+expressApp.post(
+  "/invoice",
+  bodyParser.json(),
+  expressjwt({
+    secret: "dhika-jelek",
+    algorithms: ["HS256"],
+  }),
+  createInvoiceHandler
+);
+
+expressApp.post(
+  "/refund",
+  bodyParser.json(),
+  expressjwt({
+    secret: "dhika-jelek",
+    algorithms: ["HS256"],
+  }),
+  createRefundHandler
+);
