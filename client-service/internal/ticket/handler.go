dd3f0aace757be3011eed76f8ceea88e64c06602
acomarcho <acoxstpd@gmail.com>
2023-11-29 10:47:13 +0700

    Refactor: No need to handle error status at CreateTicketHandler

--- client-service/internal/ticket/handler.go
+++ client-service/internal/ticket/handler.go
@@ -111,14 +111,12 @@ func (h *Handle) CreateTicketHandler(c echo.Context) error {
 	reserveReq.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
 
 	client := &http.Client{}
-	reserveResp, err := client.Do(reserveReq) // NOTE: An error is returned if caused by client policy (such as CheckRedirect), or failure to speak HTTP (such as a network connectivity problem). A non-2xx status code doesn't cause an error.
+	reserveResp, err := client.Do(reserveReq)
 	if err != nil {
 		h.logger.Error(err)
 		return c.JSON(http.StatusInternalServerError, handler.ErrorResponse{Message: "Failed to send reserve request"})
 	}
 	defer reserveResp.Body.Close()
-
-	// TODO: Handle if the status code is not 200
 
 	return c.JSON(http.StatusCreated, handler.SuccessResponse{Data: res})
 }
